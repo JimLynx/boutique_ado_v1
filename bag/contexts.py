e530879d82476f727ae65c073d728a13e80ab50f
JimLynx <jim.lynx@gmail.com>
2021-01-20 10:06:02 +0000

    Add adjust bag and remove from bag views, and calc ubtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,43 +3,29 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
-    # empty bag for products to go 
     bag_items = []
-
-    # default zero for total cost and product count
     total = 0
     product_count = 0
-
-    # Getting bag it if it already exists or initializing it to an empty dictionary if not
     bag = request.session.get('bag', {})
-    
+
     for item_id, item_data in bag.items():
-        # if integer we know its a quantity
         if isinstance(item_data, int):
-            # get the product
             product = get_object_or_404(Product, pk=item_id)
-            # add its quantity times the price to the total
             total += item_data * product.price
-            # increment the product count by the quantity
             product_count += item_data
-            #  dictionary containing id, quantity & product object
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
             })
-        # if not integer, then its in dictionary
         else:
-            # iterate through the inner dictionary of items_by_size
-            # incrementing the product count and total accordingly.
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
-                #for each item add the size to the 
-                # bag items returned to the template 
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
@@ -47,27 +33,16 @@ def bag_contents(request):
                     'size': size,
                 })
 
-
-    # give free delivery if spend more than amount
-    # specified in free delivery threshold in settings.py
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE / 100)
-
-        # let user know how much more to spend
-        # to get free delivery 
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        # If the total is greater than or equal to the threshold
-        # set delivery and the free_delivery_delta to zero
         delivery = 0
         free_delivery_delta = 0
 
-    # calculate grand total by adding delivery charge to the total
     grand_total = delivery + total
 
     context = {
-        # everything in this context will be available in every template.
-        # In every app across the entire project.
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
+
 def view_bag(request):
     """ A view to return the shopping bag page """
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of specified product to shopping bag """
 
-    # get quantity from form 
+    # get quantity from form
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -24,7 +27,7 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if size:
-        # If the item is already in the bag 
+        # If the item is already in the bag
         if item_id in list(bag.keys()):
             # check if another item of the same id and same size already exists.
             if size in bag[item_id]['items_by_size'].keys():
@@ -39,7 +42,7 @@ def add_to_bag(request, item_id):
     # if there's no size, run this logic
     else:
         if item_id in list(bag.keys()):
-            # update quantity of item if it exists in the bag 
+            # update quantity of item if it exists in the bag
             bag[item_id] += quantity
         else:
             # or add to bag if doesn't exist
@@ -49,3 +52,62 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
 
     return render(request, 'bag/bag.html')
+
+
+def adjust_bag(request, item_id):
+    """ Adjust a quantity of specified product to shopping bag """
+
+    # get quantity from form
+    quantity = int(request.POST.get('quantity'))
+
+    # set size initial to None
+    # if size from form, then qual that
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    # Check to see if there's a bag variable in the session.
+    # & if not, create one
+    bag = request.session.get('bag', {})
+
+    if size:
+        # if quantity is greater than zero set items accordingly
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+
+    # if there's no size, run this logic
+    else:
+        # else remove item with pop function
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
