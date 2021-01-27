fb54258e5956b89aa2f7f0005993ad51a8173cb8
JimLynx <jim.lynx@gmail.com>
2021-01-27 16:40:20 +0000

    Add default from email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,5 +185,7 @@ STRIPE_CURRENCY = "usd"
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('whsec_e8DVIfrIHur2wkPJqruyWycVK9CWDHrC', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
 
 
